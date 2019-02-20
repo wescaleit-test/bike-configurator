2634d762050eb46252b4fbb94c5fde196b9753a7
sumit <KOL0100@KOL-LAP-112.mzsk.local>
2019-02-20 10:59:01 +0530

    Test Comment

--- Bike-Configuration/src/main/java/com/javahelps/restservice/Application.java
+++ Bike-Configuration/src/main/java/com/javahelps/restservice/Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
-
+/*
+ * Starting Application Class
+ */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
