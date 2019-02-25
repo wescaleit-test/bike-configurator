21a069ebd6c877db210caa86b17d4548721c7523
sumit <KOL0100@KOL-LAP-112.mzsk.local>
2019-02-25 16:06:32 +0530

    Application Page Code Changes

--- BikConfig/src/main/java/com/bike/configurations/Application.java
+++ BikConfig/src/main/java/com/bike/configurations/Application.java
@@ -3,9 +3,9 @@ package com.bike.configurations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication(scanBasePackages = {"com.bike.configurations.controller"})
-@ComponentScan("com.bike.configurations.repository")
+@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = {"com.bike.configurations.controller"})
+/* @ComponentScan("com.bike.configurations.repository") */
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 /*
